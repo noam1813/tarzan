bfb2fe96e2fb8567707e48735f906afcef65ab07
norhum/gobouman <devgobouman@gmail.com>
2020-11-23 13:04:46 +0900

    Gitへ保存

---
+++ Tarzan/Assets/FireManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FireManager : MonoBehaviour
+{
+    public static FireManager instance;
+    [SerializeField] private GameObject firePrefab;
+    [SerializeField] private float interval;
+
+    [SerializeField] private Vector2 LeftTopPos;
+    [SerializeField] private Vector2 RightBottomPos;
+
+    private IDisposable spawnObservable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        spawnObservable = Observable.Interval(TimeSpan.FromSeconds(interval)).Subscribe(_ =>
+        {
+            Spawn();   
+        }).AddTo(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Spawn()
+    {
+        var mode = Random.Range(0, 4);
+        var mode2 = -1+Random.Range(0, 2)*2;
+        var percent = Random.Range(0f, 1f);
+        Vector3 pos;
+
+        float posx;
+        float posy;
+        switch (mode)
+        {
+            //上
+            case 0:
+                posx = RightBottomPos.x * mode2 - RightBottomPos.x * 2 * mode2 * percent;
+                pos = new Vector3(posx,LeftTopPos.y);
+                break;
+            //下
+            case 1:
+                posx = RightBottomPos.x * mode2 - RightBottomPos.x * 2 * mode2 * percent;
+                pos = new Vector3(posx,RightBottomPos.y);
+                break;
+            //左
+            case 2:
+                posy = RightBottomPos.y * mode2 - RightBottomPos.y * 2 * mode2 * percent;
+                pos = new Vector3(LeftTopPos.x,posy);
+                break;
+            //右
+            case 3:
+                posy = RightBottomPos.y * mode2 - RightBottomPos.y * 2 * mode2 * percent;
+                pos = new Vector3(RightBottomPos.x,posy);
+                break;
+        }
+        Debug.Log("OK");
+    }
+}
---
+++ Tarzan/Assets/Tarzan.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tarzan : MonoBehaviour
+{
+    private Vector2 mousePosition;
+    private Vector3 Speed;
+    [SerializeField] private Vector2 SpeedAccel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        var pos = transform.position;
+        
+        //Xziku
+        if (pos.x >= mousePosition.x)
+        {
+            Speed.x -= SpeedAccel.x * Time.deltaTime;
+        }
+        else
+        {
+            Speed.x += SpeedAccel.x * Time.deltaTime;
+        }
+        
+        //Yziku
+        if (pos.y >= mousePosition.y)
+        {
+            Speed.y -= SpeedAccel.y * Time.deltaTime;
+        }
+        else
+        {
+            Speed.y += SpeedAccel.y * Time.deltaTime;
+        }
+
+        transform.position += Speed * Time.deltaTime;
+    }
+}
