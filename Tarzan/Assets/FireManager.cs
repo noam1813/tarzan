6a56bffcf3b5c0571ad1d8de0e584801bfcfdc05
Noam1813 <13.noam.18@gmail.com>
2020-11-28 14:23:42 +0900

    微調整

--- Tarzan/Assets/FireManager.cs
+++ Tarzan/Assets/FireManager.cs
@@ -39,33 +39,40 @@ public class FireManager : MonoBehaviour
         var mode = Random.Range(0, 4);
         var mode2 = -1+Random.Range(0, 2)*2;
         var percent = Random.Range(0f, 1f);
-        Vector3 pos;
+        Vector3 pos = Vector3.zero;
 
         float posx;
         float posy;
-        switch (mode)
+        switch (1)
         {
             //上
             case 0:
                 posx = RightBottomPos.x * mode2 - RightBottomPos.x * 2 * mode2 * percent;
+                posx = Mathf.RoundToInt(posx);
                 pos = new Vector3(posx,LeftTopPos.y);
                 break;
             //下
             case 1:
                 posx = RightBottomPos.x * mode2 - RightBottomPos.x * 2 * mode2 * percent;
+                posx = Mathf.RoundToInt(posx);
                 pos = new Vector3(posx,RightBottomPos.y);
                 break;
             //左
             case 2:
                 posy = RightBottomPos.y * mode2 - RightBottomPos.y * 2 * mode2 * percent;
+                posy = Mathf.RoundToInt(posy);
                 pos = new Vector3(LeftTopPos.x,posy);
                 break;
             //右
             case 3:
                 posy = RightBottomPos.y * mode2 - RightBottomPos.y * 2 * mode2 * percent;
+                posy = Mathf.RoundToInt(posy);
                 pos = new Vector3(RightBottomPos.x,posy);
                 break;
         }
+
+        var obj = Instantiate(firePrefab);
+        obj.transform.position = pos;
         Debug.Log("OK");
     }
 }
